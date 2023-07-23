a0f13f309d934a63e602ccb393cf8e3d0c452ea5
김민재 <poodle7192@gmail.com>
2023-07-23 23:59:46 +0900

    mysql connect make get,post api

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,32 +3,42 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate; //jdbc Template 이용 -> SQL문 실행 가능
+
+    public UserController(JdbcTemplate jdbcTemplate){ //생성자를 만들어 jdbcTemplate를 파라미터로 넣으면, 자동으로 들어옴
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request){
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user(name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge()); //insert, update, delete query에 사용 가능
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers(){
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++){
-            responses.add(new UserResponse(i+1, users.get(i)));
-        }
-
-        return responses;
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
     }
 
 }
