cf4941200a55dbadaeb9fe430d645e7926297da2
김민재 <poodle7192@gmail.com>
2023-07-25 23:40:42 +0900

    controller, service, repository 분류, 역할별로 기능 수행하도록 수정

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,61 +3,39 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController //API 진입 지점으로써 HTTP Body를 객체로 변당
 public class UserController {
 
-    private final JdbcTemplate jdbcTemplate; //jdbc Template 이용 -> SQL문 실행 가능
+    private final UserService userService;
 
     public UserController(JdbcTemplate jdbcTemplate){ //생성자를 만들어 jdbcTemplate를 파라미터로 넣으면, 자동으로 들어옴
-        this.jdbcTemplate = jdbcTemplate;
+        this.userService = new UserService(jdbcTemplate);
     }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request){
-        String sql = "INSERT INTO user(name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge()); //insert, update, delete query에 사용 가능
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers(){
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request){
-        String reqdSql = "SELECT * FROM user WHERE id = ?";
-        boolean isUserNotExist = jdbcTemplate.query(reqdSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userService.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name){
-        String readSql = "SELECT * FROM user WHERE name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "DELETE FROM user WHERE name = ?";
-        jdbcTemplate.update(sql, name);
+        userService.deleteUser(name);
     }
 
 }
---
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -0,0 +1,45 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserService { //Service : 분기 처리 및 로직 담당
+
+    private final UserRepository userRepository;
+
+    public UserService(JdbcTemplate jdbcTemplate){
+        userRepository = new UserRepository(jdbcTemplate);
+    }
+
+    public void saveUser(UserCreateRequest request){
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers()
+    {
+        return userRepository.getUser();
+    }
+
+    public void updateUser(UserUpdateRequest request){
+
+        if (userRepository.isUserNotExist(request.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
+
+    public void deleteUser(String name){
+
+        if (userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.deleteUser(name);
+    }
+}
