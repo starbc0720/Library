21598a11a9a0b6849c716d8e558d25c9173a7869
김민재 <poodle7192@gmail.com>
2023-08-02 23:38:46 +0900

    도서 대출 api 생성

---
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -0,0 +1,30 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserLoanHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    private Long userId;
+
+    private String bookName;
+
+    private boolean isReturn; //boolean으로 처리하면 tinyint에 잘 매핑(0 -> false, 1 -> true)
+
+    public UserLoanHistory() {
+    }
+
+    public UserLoanHistory(Long userId, String bookName) {
+        this.userId = userId;
+        this.bookName = bookName;
+        this.isReturn = false;
+    }
+
+}
--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -2,7 +2,12 @@ package com.group.libraryapp.service.book;
 
 import com.group.libraryapp.domain.book.Book;
 import com.group.libraryapp.domain.book.BookRepository;
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.domain.user.UserRepository;
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository;
 import com.group.libraryapp.dto.book.request.BookCreateRequest;
+import com.group.libraryapp.dto.book.request.BookLoanRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,13 +15,41 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final UserLoanHistoryRepository userLoanHistoryRepository;
+    private final UserRepository userRepository;
 
-    public BookService(BookRepository bookRepository) {
+    public BookService(
+            BookRepository bookRepository,
+            UserLoanHistoryRepository userLoanHistoryRepository,
+            UserRepository userRepository) {
         this.bookRepository = bookRepository;
+        this.userLoanHistoryRepository = userLoanHistoryRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
     public void saveBook(BookCreateRequest request){
         bookRepository.save(new Book(request.getName()));
+    }
+
+    @Transactional
+    public void loanBook(BookLoanRequest request){
+        // 1. 책 정보 가져옴
+        Book book = bookRepository.findByName(request.getBookName())
+                .orElseThrow(IllegalArgumentException::new);
+
+        // 2. 대출기록 정보 확인 대출중인지 확인
+        if (userLoanHistoryRepository.existsByBookNameAndIsReturn(book.getName(), false)){
+            // 3. 확인 시 대출 중이라면 예외 발생
+            throw new IllegalArgumentException("이미 대출되어 있는 책입니다");
+        }
+
+        // 4. 유저 정보를 가져옴
+        User user = userRepository.findByName(request.getUserName())
+                .orElseThrow(IllegalArgumentException::new);
+
+        // 5. 유저 정보와 책 정보를 기반으로 UserLoanHistory 저장
+        userLoanHistoryRepository.save(new UserLoanHistory(user.getId(), book.getName()));
+
     }
 }
