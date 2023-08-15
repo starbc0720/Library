e7a0b5cec4752af3d7cba76badf07eb76f872088
김민재 <poodle7192@gmail.com>
2023-08-15 23:56:02 +0900

    profile 설정 통해 local, dev 환경에 따라 다르게 구동 되도록 설정

--- src/main/java/com/group/libraryapp/config/UserConfiguration.java
+++ src/main/java/com/group/libraryapp/config/UserConfiguration.java
@@ -1,9 +1,9 @@
 package com.group.libraryapp.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.userService;
 
-@Configuration //@Configuration + @Bean -> 외부 라이브러리, 프레임워크에서 만든 클래스 등록 시 사용
-public class UserConfiguration {
+@userService //@userService + @Bean -> 외부 라이브러리, 프레임워크에서 만든 클래스 등록 시 사용
+public class UseruserService {
 
 //    @Bean
 //    public UserJdbcRepository userRepository(JdbcTemplate jdbcTemplate){
